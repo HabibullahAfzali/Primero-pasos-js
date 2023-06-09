45fc658ec8dcaf59a82a846756641f36f0d15357
HabibullahAfzali <hbafzali@gmail.com>
2023-06-09 19:22:38 +0200

    Adding function, with different data manipulation!

---
+++ main.js
@@ -0,0 +1,12 @@
+// <-- adding function // the below function sum the given parameters--->
+function addNumbers(a, b) {
+  let result = a + b;
+  console.log("The sum of two given numbers are= " + result);
+  console.log(typeof result);
+}
+addNumbers(12 + 5);
+console.log(typeof addNumbers);
+addNumbers(12 * 30 + 5, "   Days of the Years");
+console.log(typeof addNumbers);
+addNumbers("A" + "F");
+console.log(typeof addNumbers);
